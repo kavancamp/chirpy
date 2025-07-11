8316c6aa4414fb7c5b5d1fae1aeddf92d5ba56a7
kavancamp <keenahvancampenhout@gmail.com>
2025-07-11 14:40:24 -0500

    added handle create users

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"chirpy/internal/database"
-	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,26 +8,36 @@ import (
 	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 
+	"database/sql"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	DB             *database.Queries
-
+	Platform        string
 }
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Add(1)
-	next.ServeHTTP(w, r)
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
 	})
 }
 
 func (cfg *apiConfig) adminMetricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	count := cfg.fileserverHits.Load()
 	html := fmt.Sprintf(`
 	<html>
@@ -45,9 +52,19 @@ func (cfg *apiConfig) adminMetricsHandler(w http.ResponseWriter, r *http.Request
 }
 
 func (cfg *apiConfig) adminResetHandler(w http.ResponseWriter, r *http.Request) {
+	if cfg.Platform != "dev" {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	err := cfg.DB.DeleteAllUsers(r.Context())
+	if err != nil {
+		log.Printf("Error deleting users: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Failed to reset users")
+		return
+	}
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "Hits counter reset to 0\n")
 }
 
@@ -55,7 +72,8 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	resp := map[string]string{"error": msg}
 	respondWithJSON(w, code, resp)
 }
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	d, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling response: %s", err)
@@ -102,6 +120,39 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	resp := CleanedResponse{CleanedBody: cleaned}
 	respondWithJSON(w, http.StatusOK, resp)
 }
+
+func (cfg *apiConfig) handleCreateUser(w http.ResponseWriter, r *http.Request) {
+	type userInput struct {
+		Email string `json:"email"`
+	}
+
+	var input userInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Request")
+		return
+	}
+	if strings.TrimSpace(input.Email) == "" {
+		respondWithError(w, http.StatusBadRequest, "Email is required")
+		return
+	}
+
+	dbUser, err := cfg.DB.CreateUser(r.Context(), input.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Could not create user")
+		return
+	}
+	user := User {
+		ID: dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Email: dbUser.Email,
+	}
+	respondWithJSON(w, http.StatusCreated, user)
+}
+
+
+
 func main() {
 	// Load .env file
 	err := godotenv.Load()
@@ -117,29 +168,28 @@ func main() {
 	}
 
 	defer db.Close()
-	
-	dbQueries := database.New(db)
-	apiCfg := apiConfig{
-		DB: dbQueries,
-	}
-	_, err = apiCfg.DB.CreateUser(context.Background(), "test@example.com")
-	if err != nil {
-		log.Println("error creating user:", err)
-	}
 
+	dbQueries := database.New(db)
+	cfg := apiConfig{
+		DB: dbQueries,
+		Platform: os.Getenv("PLATFORM"),
+	}
 	mux := http.NewServeMux()
-	//readiness endpoint 
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request){
+	//readiness endpoint
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "OK")
 	})
-	mux.HandleFunc("GET /admin/metrics", apiCfg. adminMetricsHandler)
-	mux.HandleFunc("POST /admin/reset", apiCfg.adminResetHandler)
+
+	mux.HandleFunc("GET /admin/metrics", cfg.adminMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.adminResetHandler)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", cfg.handleCreateUser)
+	mux.HandleFunc("POST /api/chirps", cfg.handleChirps)
 	// File server wrapped with metrics middleware
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileServer)))
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(fileServer)))
 
 	// start the server on port 8089
 	log.Println("Server running on http://localhost:8080")
@@ -147,6 +197,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
-
