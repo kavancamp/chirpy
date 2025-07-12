982c27657c5f3e8a9481bf60c9bba9a87de93b97
kavancamp <keenahvancampenhout@gmail.com>
2025-07-11 19:31:27 -0500

    updated users query and added get by emaik

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,11 +1,10 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, email)
-VALUES (
-    gen_random_uuid(), NOW(), NOW(), $1
-)
+INSERT INTO users (id, email, hashed_password, created_at, updated_at)
+VALUES (gen_random_uuid(), $1, $2, NOW(), NOW())
 RETURNING *;
-
 
 -- name: DeleteAllUsers :exec
 DELETE FROM users;
 
+-- name: GetUserByEmail :one
+SELECT * FROM users WHERE email = $1;
