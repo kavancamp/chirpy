1d98a9bacd03a45d2478f51123da57605433e85f
kavancamp <keenahvancampenhout@gmail.com>
2025-07-11 21:55:36 -0500

    added auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,13 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+	"errors"
+	
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error){
 	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -12,4 +19,49 @@ func HashPassword(password string) (string, error){
 
 func CheckPasswordHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+var (
+	errInvalidToken = errors.New("invalid token")
+)
+
+// creates and signs a new JWT for a user
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+// validates a JWT and returns the user ID if successful
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	parsedToken, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errInvalidToken
+		}
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, errInvalidToken
+	}
+
+	claims, ok := parsedToken.Claims.(*jwt.RegisteredClaims)
+	if !ok || !parsedToken.Valid {
+		return uuid.Nil, errInvalidToken
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.Nil, errInvalidToken
+	}
+
+	return userID, nil
 }
