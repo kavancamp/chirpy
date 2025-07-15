7c82726652271a2962a153263b0a5606b29d2e6c
kavancamp <keenahvancampenhout@gmail.com>
2025-07-15 15:11:19 -0500

    added query to delete by id and get by author id

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -10,3 +10,11 @@ ORDER BY created_at ASC;
 -- name: GetChirpsByID :one
 SELECT * FROM chirps
 WHERE id = $1;
+
+-- name: DeleteChirpByID :exec
+DELETE FROM chirps WHERE id = $1;
+
+-- name: GetChirpsByAuthorID :many
+SELECT * FROM chirps
+WHERE user_id = $1
+ORDER BY created_at ASC;
